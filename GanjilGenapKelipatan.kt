982c5fc21f814d965c633fe4777ee8ffed9400e6
kharozim <kharozim06@gmail.com>
2020-11-04 22:04:37 +0700

    update ganjil genap kelipatan

--- GanjilGenapKelipatan.kt
+++ GanjilGenapKelipatan.kt
@@ -1,8 +1,19 @@
 package kotlin_basic_exercise
 
 fun main() {
+    scan(1, 1000)
+}
 
+fun scan(awal: Int, akhir: Int) {
+    for (i in awal..akhir) {
 
+        when {
+            i % 100 == 0 -> println("$i kelipatan 100")
+            i % 2 != 0 && i % 7 == 0 -> println("$i Ganjil kelipatan tujuh")
+            i % 2 == 0 && i % 8 == 0 -> println("$i Genap kelipatan delapan")
+            i % 2 != 0 -> println("$i Ganjil")
+            i % 2 == 0 -> println("$i Genap")
+        }
 
-
+    }
 }

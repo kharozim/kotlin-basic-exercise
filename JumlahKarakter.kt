ffdf448696c5a9fc57754e70f2698b835e31667e
kharozim <kharozim06@gmail.com>
2020-11-05 00:04:54 +0700

    Edit JumlahKarakter
    Signed-off-by: kharozim <kharozim06@gmail.com>

--- JumlahKarakter.kt
+++ JumlahKarakter.kt
@@ -1,8 +1,17 @@
 package kotlin_basic_exercise
 
 fun main() {
-    var kata : String = "Ilmiah"
-    val chararray = kata.toCharArray()
-    println(chararray.indices.endInclusive + 1) //cara 1
-    println(chararray.lastIndex + 1) // cara 2
+    var kata: String = "Ilmiah"
+    var kata2: String = "Lari Pagi"
+    val charArray1 = kata.toCharArray()
+    val charArray2 = kata2
+        .replace("\\s".toRegex(), "") //menghilangkan whitespace
+        .toCharArray()
+
+    println(charArray1.indices.endInclusive + 1) //cara 1
+    println(charArray1.lastIndex + 1) // cara 2
+
+
+    println(charArray2.indices.endInclusive + 1)//cara 1
+    println(charArray2.lastIndex + 1)// cara 2
 }

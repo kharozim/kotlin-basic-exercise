d0a064da93c9cfefb13c3f84b73234ffa2f70f5b
kharozim <kharozim06@gmail.com>
2020-11-04 22:29:54 +0700

    update ganjil genap kelipatan (2 cara)

--- GanjilGenapKelipatan.kt
+++ GanjilGenapKelipatan.kt
@@ -1,19 +1,36 @@
 package kotlin_basic_exercise
 
 fun main() {
-    scan(1, 1000)
+    scan(1, 10)
 }
 
 fun scan(awal: Int, akhir: Int) {
+    var result :String
     for (i in awal..akhir) {
 
-        when {
+
+        //cara pertama
+        if (i % 2 != 0) {
+            if (i % 7 == 0) {
+                result = "$i Ganjil kelipatan tujuh"
+            } else result = "$i ganjil"
+
+        } else {
+            result = "$i Genap"
+            if (i % 8 == 0) {
+                result = "$i Genap kelipatan delapan"
+            }
+        }
+        println(result)
+
+//cara kedua
+      /*  when {
             i % 100 == 0 -> println("$i kelipatan 100")
             i % 2 != 0 && i % 7 == 0 -> println("$i Ganjil kelipatan tujuh")
             i % 2 == 0 && i % 8 == 0 -> println("$i Genap kelipatan delapan")
             i % 2 != 0 -> println("$i Ganjil")
             i % 2 == 0 -> println("$i Genap")
-        }
+        }*/
 
     }
 }
